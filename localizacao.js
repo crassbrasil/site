8fd0679c1ed0bf7bf6998156d3020ba258335e75
crassbrasil <147461200+crassbrasil@users.noreply.github.com>
2025-05-09 09:51:29 -0300

    Update localizacao.js

--- localizacao.js
+++ localizacao.js
@@ -1,7 +1,20 @@
-$.getJSON("https://wtfismyip.com/json", l => {
-  const regiao = l['YourFuckingLocation']?.replace(", Brazil", "") || "sua região";
-  const destino = document.getElementById("localizacao-dinamica");
-  if (destino) {
-    destino.innerHTML = 'Localização detectada: <b style="color:#36b376">' + regiao + '</b>';
+(function() {
+  function inserirLocalizacao() {
+    $.getJSON("https://wtfismyip.com/json", function(l) {
+      const regiao = l['YourFuckingLocation']?.replace(", Brazil", "") || "sua região";
+      const destino = document.getElementById("localizacao-dinamica");
+      if (destino) {
+        destino.innerHTML = 'Localização detectada: <b style="color:#36b376">' + regiao + '</b>';
+      }
+    });
   }
-});
+
+  if (window.jQuery) {
+    inserirLocalizacao();
+  } else {
+    const script = document.createElement('script');
+    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+    script.onload = inserirLocalizacao;
+    document.head.appendChild(script);
+  }
+})();
