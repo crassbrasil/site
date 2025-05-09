f17dea7f20dbf8c28a0b69f707d188059c47eb36
crassbrasil <147461200+crassbrasil@users.noreply.github.com>
2025-05-09 09:33:24 -0300

    Update localizacao.js

--- localizacao.js
+++ localizacao.js
@@ -1,3 +1,7 @@
 $.getJSON("https://wtfismyip.com/json", l => {
-  document.write("Localização detectada: <b style='color:#36b376'>" + l['YourFuckingLocation'].replace(", Brazil", "") + "</b>");
+  const regiao = l['YourFuckingLocation']?.replace(", Brazil", "") || "sua região";
+  const destino = document.getElementById("localizacao-dinamica");
+  if (destino) {
+    destino.innerHTML = 'Localização detectada: <b style="color:#36b376">' + regiao + '</b>';
+  }
 });
